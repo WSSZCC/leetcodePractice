171b37437a0a5324ce646f668f8240c293be8fce
xsl <x18573937220@163.com>
2024-07-04 12:31:56 +0800

    设置.gitignore文件

--- src/test/practice/WaitNotifyDemo.java
+++ src/test/practice/WaitNotifyDemo.java
@@ -1,12 +1,10 @@
 package test.practice;
 
-import java.util.concurrent.locks.Lock;
-
 public class WaitNotifyDemo {
     public static void main(String[] args) {
         Object lock = new Object();
-        new Thread(()->{
-            synchronized (lock){
+        new Thread(() -> {
+            synchronized (lock) {
                 System.out.println("我要开始等待1");
                 try {
                     lock.wait();
@@ -16,8 +14,8 @@ public class WaitNotifyDemo {
                 System.out.println("我要被叫醒了1");
             }
         }).start();
-        new Thread(()->{
-            synchronized (lock){
+        new Thread(() -> {
+            synchronized (lock) {
                 System.out.println("我要被唤醒2");
                 lock.notify();
                 System.out.println("我已经被叫醒了2");
