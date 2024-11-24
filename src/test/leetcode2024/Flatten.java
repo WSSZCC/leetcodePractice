b5af7de0d9d72d18e940469b1f6ae1a1bfa56145
xsl <x18573937220@163.com>
2024-11-24 21:24:30 +0800

    每天一练  二叉树展开为链表

--- src/test/leetcode2024/Flatten.java
+++ src/test/leetcode2024/Flatten.java
@@ -8,45 +8,29 @@ import test.entry.TreeNode;
 public class Flatten {
 
     TreeNode node = new TreeNode();
-    int count = 0;
+    TreeNode tree = null;
+
     public void flatten(TreeNode root) {
-        if(root==null){
+        if (root == null) {
             return;
         }
-        TreeNode tree = node;
-        dfs(tree,root);
-        root.right = node.right;
+        tree = node;
+        dfs(root);
+        root.right = node.right.right;
         root.left = null;
-
-        while (node!=null){
-            System.out.println(node.val);
-            node = node.right;
-        }
 
     }
 
-    private void dfs(TreeNode tree,TreeNode root) {
+    private void dfs(TreeNode root) {
         TreeNode right = root.right;
         TreeNode left = root.left;
         tree.right = new TreeNode(root.val);
-        tree.left = null;
         tree = tree.right;
-        printNode();
-        if(left!=null){
-            dfs(tree,left);
+        if (left != null) {
+            dfs(left);
         }
-        if(right!=null){
-            dfs(tree,right);
-        }
-    }
-
-    private void printNode() {
-        TreeNode node1 = node;
-        count++;
-        System.out.println("---------"+count+"--------");
-        while (node1!=null){
-            System.out.println(node1.val);
-            node1 = node1.right;
+        if (right != null) {
+            dfs(right);
         }
     }
 
