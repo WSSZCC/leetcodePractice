fd57298e47e013eacab61949424f356a1a1903a5
xsl <x18573937220@163.com>
2025-04-06 00:02:12 +0800

    每天一练  找出所有子集的异或总和再求和

---
+++ src/test/leetcode2025/SubsetXORSum.java
@@ -0,0 +1,26 @@
+package test.leetcode2025;
+
+/**
+ *
+ */
+public class SubsetXORSum {
+    int ans;
+    public int subsetXORSum(int[] nums) {
+        ans = 0;
+        int n = nums.length;
+        backtrack(nums,n,0,0);
+        return ans;
+    }
+
+    private void backtrack(int[] nums, int n, int i,int value) {
+        if(i==n){
+            return;
+        }
+        for (int j = i; j < n; j++) {
+            value^=nums[j];
+            ans+=value;
+            backtrack(nums, n, j+1, value);
+            value^=nums[j];
+        }
+    }
+}
