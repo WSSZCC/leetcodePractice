2810a95b5efd5526f5f0e80f4a2de2ed861641d4
xsl <x18573937220@163.com>
2025-04-20 22:23:53 +0800

    每天一练 森林中的兔子

---
+++ src/test/leetcode2025/NumRabbits.java
@@ -0,0 +1,25 @@
+package test.leetcode2025;
+
+
+/**
+ * 森林中的兔子
+ */
+public class NumRabbits {
+    public int numRabbits(int[] answers) {
+        int[] ints = new int[1000];
+        int ans = 0;
+        for (int answer : answers) {
+           if(answer==0){
+               ans++;
+               continue;
+           }
+           if(ints[answer]==0){
+               ans+=answer+1;
+               ints[answer]=answer;
+               continue;
+           }
+           ints[answer]--;
+        }
+        return ans;
+    }
+}
